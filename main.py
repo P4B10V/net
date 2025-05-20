01e8728ea421f8f9851f6e207d9a7ac2778a86a2
P4B10V <peuvebe16@gmail.com>
2025-05-20 18:43:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ## SNIFFER ##
 
 import argparse
-from scapy.all import *
+from scapy.all import ARP, Ether, srp
 
 parser = argparse.ArgumentParser(prog='~ SNIFFER ~',
                                description='Proyecto personal para crear un sniffer con Python/Scapy')
@@ -11,19 +11,19 @@ parser.add_argument('-s', '--scan', action='store_true', help='Escanear la red e
 
 args = parser.parse_args()
 
-
 #Lo primero que quiero hacer es un escaneo de mi red para ver que dispositivos están activos, por lo que la primera funcionalidad
 #será hacer un escaneo de red. La opción elegida será enviar un ARP y ver que respuestas tengo.
 def escaneo():
 
-        arp = ARP(op=1, pdst="10.22.0.0/16")
+        arp = ARP(op=1, pdst="10.22.0.0/16") #op=1 == who-has, op=2 == is-at
         ether = Ether(dst="ff:ff:ff:ff:ff:ff")
         paquete = ether / arp
-
+#La información que contiene paquete, si lo utilizamos con Scapy, podemos verla con paquete[0].show() y paquete[1].show()
+# [0] es la capa de Ethernet y [1] la capa ARP
+  
         dispositivos = []
 
         escaneo_red = srp(paquete,verbose=0,timeout=5)[0]
-
         for enviado, recibido in escaneo_red:
                 dispositivos.append(recibido.psrc)
 
